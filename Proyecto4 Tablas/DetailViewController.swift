4e3f4ac6714a297cecc587ce623363b084e0d4c4
marialacayo <maria@dwim.mx>
2021-03-02 21:03:10 -0600

    first commit

---
+++ Proyecto4 Tablas/DetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DetailViewController.swift
+//  Proyecto4 Tablas
+//
+//  Created by Maria Lacayo on 22/02/20.
+//  Copyright © 2020 dwim. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
